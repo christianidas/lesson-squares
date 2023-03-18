80b6e58b52d773470df6855f03457c194f0d1499
Christian Baird <christian.a.baird@gmail.com>
2023-03-18 15:04:10 -0400

    Adding String[][] variable for grid

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] grid = new String[10][10];
+
         System.out.println("Hello world!");
     }
 }
