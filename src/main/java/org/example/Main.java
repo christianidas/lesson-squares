b4bba692b1e0b4480e52a4387b8f3c8aa7650596
Christian Baird <christian.a.baird@gmail.com>
2023-03-18 15:27:45 -0400

    Looping through the grid and prompting for name for each square which is annoying

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String[][] grid = new String[10][10];
 
-        addSquare(grid, 0, 0, "CB");
-        addSquare(grid, 3, 5, "CB");
-        addSquare(grid, 1, 4, "CW");
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.printf("Enter a name for row %d col %d: ", i, j);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String name = bufferedReader.readLine();
+                addSquare(grid, i, j, name);
+            }
+        }
 
         printGrid(grid);
     }
