aef9c659aa3c456a6ca1f676190eaacaac5f59fc
Christian Baird <christian.a.baird@gmail.com>
2023-03-22 23:57:49 -0400

    Adding Square class

---
+++ src/main/java/org/example/Grid/Square.java
@@ -0,0 +1,19 @@
+package org.example.Grid;
+
+import org.example.Person.Person;
+
+public class Square {
+    private Person owner;
+
+    public Square() {
+
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+}
