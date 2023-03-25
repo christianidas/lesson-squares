fcf6dc1aa6f14085ca4459ab1676dfa363cf7605
Christian Baird <christian.a.baird@gmail.com>
2023-03-25 13:58:49 -0400

    Creating an empty grid list to start

--- src/main/java/org/example/Grid/service/GridService.java
+++ src/main/java/org/example/Grid/service/GridService.java
@@ -1,4 +1,10 @@
 package org.example.Grid.service;
 
+import org.example.Grid.entity.Grid;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GridService {
+    private final List<Grid> grids = new ArrayList<>();
 }
