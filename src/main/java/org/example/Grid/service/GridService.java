701bbf8a6b8b3c3c5aa8f8f688cc3eccb48d512d
Christian Baird <christian.a.baird@gmail.com>
2023-03-25 14:04:14 -0400

    Adding basic CRUD (Create, Retrieve, Update, Delete) operations

--- src/main/java/org/example/Grid/service/GridService.java
+++ src/main/java/org/example/Grid/service/GridService.java
@@ -7,4 +7,27 @@ import java.util.List;
 
 public class GridService {
     private final List<Grid> grids = new ArrayList<>();
+
+    public Grid create(Grid grid) {
+        grids.add(grid);
+        return grid;
+    }
+
+    public List<Grid> getAll() {
+        return grids;
+    }
+
+    public Grid get(int index) {
+        return grids.get(index);
+    }
+
+    public Grid update(int index, Grid grid) {
+        Grid res = get(index);
+        res.setSquares(grid.getSquares());
+        return res;
+    }
+
+    public void delete(int index) {
+        grids.remove(index);
+    }
 }
