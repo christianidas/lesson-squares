4704f6400e8d682e8a5ace680265a66f94e226d3
Christian Baird <christian.a.baird@gmail.com>
2023-03-25 14:24:45 -0400

    Changing our command structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import org.example.Grid.entity.Grid;
+import org.example.Grid.service.GridService;
+import org.example.Person.entity.Person;
+import org.example.Person.service.PersonService;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,39 +13,71 @@ public class Main {
     private static final String COMMAND_MESSAGE = "Please enter a command";
 
     public static void main(String[] args) throws IOException {
-        Grid grid = new Grid();
+        final GridService gridService = new GridService();
+        final PersonService personService = new PersonService();
 
         do {
-            printGrid(grid);
-
             String command = prompt(COMMAND_MESSAGE).toLowerCase();
+            String[] parts = command.split(" ");
+            String action = parts[0];
+            String entity = parts[1];
+            int index;
 
             if (command.equals("q")) {
                 break;
             }
 
-            switch (command) {
+            switch (action) {
                 case "add":
                 case "a":
-                    int rowToAdd = Integer.parseInt(prompt("Row"));
-                    int colToAdd = Integer.parseInt(prompt("Col"));
-                    String name = prompt("Name");
-                    grid.addSquare(rowToAdd, colToAdd, name);
+                    switch (entity) {
+                        case "grid":
+                            gridService.create(new Grid());
+                            break;
+                        case "person":
+                            Person person = new Person();
+                            String name = prompt("Name");
+                            person.setName(name);
+                            personService.create(person);
+                            break;
+                        default:
+                            System.out.println("Invalid command!");
+                    }
+                    break;
+                case "get":
+                case "g":
+                    index = Integer.parseInt(parts[2]);
+                    switch (entity) {
+                        case "grid":
+                            Grid grid = gridService.get(index);
+                            System.out.println(grid);
+                            break;
+                        case "person":
+                            Person person = personService.get(index);
+                            System.out.println(person);
+                            break;
+                        default:
+                            System.out.println("Invalid command!");
+                    }
                     break;
                 case "remove":
                 case "r":
-                    int rowToRemove = Integer.parseInt(prompt("Row"));
-                    int colToRemove = Integer.parseInt(prompt("Col"));
-                    grid.removeSquare(rowToRemove, colToRemove);
+                    index = Integer.parseInt(parts[2]);
+                    switch (entity) {
+                        case "grid":
+                            gridService.delete(index);
+                            break;
+                        case "person":
+                            personService.delete(index);
+                            break;
+                        default:
+                            System.out.println("Invalid command!");
+                    }
                     break;
                 default:
                     System.out.println("Invalid command!");
             }
         } while (true);
-    }
-
-    private static void printGrid(Grid grid) {
-        System.out.print(grid);
     }
 
     private static String prompt(String message) throws IOException {
