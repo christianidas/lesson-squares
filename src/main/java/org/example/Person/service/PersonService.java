5abead8ed4e6dd8d8b9f0d2fd41cc7724e069e82
Christian Baird <christian.a.baird@gmail.com>
2023-03-25 14:06:58 -0400

    Doing the same for PersonService

--- src/main/java/org/example/Person/service/PersonService.java
+++ src/main/java/org/example/Person/service/PersonService.java
@@ -1,4 +1,33 @@
 package org.example.Person.service;
 
+import org.example.Person.entity.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonService {
+    private final List<Person> people = new ArrayList<>();
+
+    public Person create(Person person) {
+        people.add(person);
+        return person;
+    }
+
+    public List<Person> getAll() {
+        return people;
+    }
+
+    public Person get(int index) {
+        return people.get(index);
+    }
+
+    public Person update(int index, Person person) {
+        Person res = get(index);
+        res.setName(person.getName());
+        return res;
+    }
+
+    public void delete(int index) {
+        people.remove(index);
+    }
 }
