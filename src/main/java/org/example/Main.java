74c022c556bdc84d8f495a49c5c9532423acd724
Christian Baird <christian.a.baird@gmail.com>
2023-03-29 22:55:14 -0400

    Removing our existing Command Line Interface which we'll replace with Spring Boot code for a web API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,54 +1,9 @@
 package org.example;
 
-import org.example.Grid.controller.GridController;
-import org.example.Grid.service.GridService;
-import org.example.Person.controller.PersonController;
-import org.example.Person.service.PersonService;
-import org.example.util.CommandLineInterface;
-
 public class Main {
-    private static final String COMMAND_MESSAGE = "Please enter a command";
 
-    public static void main(String[] args) {
-        final GridService gridService = new GridService();
-        final PersonService personService = new PersonService();
-        final GridController gridController = new GridController(gridService, personService);
-        final PersonController personController = new PersonController(personService);
+	public static void main(String[] args) {
 
-        do {
-            String command = CommandLineInterface.prompt(COMMAND_MESSAGE).toLowerCase();
+	}
 
-            if (command.equals("q") || command.equals("quit")) {
-                break;
-            }
-
-            String[] parts = command.split(" ");
-
-            if (parts.length < 2) {
-                CommandLineInterface.print("Invalid command!");
-                continue;
-            }
-
-            String entity = parts[1];
-
-            switch (entity) {
-                case "grid":
-                    try {
-                        CommandLineInterface.print(gridController.handler(command));
-                    } catch (Exception e) {
-                        CommandLineInterface.print(e.getMessage());
-                    }
-                    break;
-                case "person":
-                    try {
-                        CommandLineInterface.print(personController.handler(command));
-                    } catch (Exception e) {
-                        CommandLineInterface.print(e.getMessage());
-                    }
-                    break;
-                default:
-                    CommandLineInterface.print("Invalid command!");
-            }
-        } while (true);
-    }
 }
