a48868eb33c48bce7a0561adf8025895495f0836
Christian Baird <christian.a.baird@gmail.com>
2023-04-01 00:14:20 -0400

    Creating abstract crud repository, service and controller that house all of our boilerplate logic

---
+++ src/main/java/org/example/crud/service/CrudService.java
@@ -0,0 +1,33 @@
+package org.example.crud.service;
+
+import org.example.crud.repository.CrudRepository;
+
+import java.util.List;
+
+public abstract class CrudService<T, ID> {
+    private final CrudRepository<T, ID> repository;
+
+    public CrudService(CrudRepository<T, ID> repository) {
+        this.repository = repository;
+    }
+
+    public T create(T entity) {
+        return repository.save(entity);
+    }
+
+    public List<T> getAll() {
+        return repository.findAll();
+    }
+
+    public T get(ID id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public T update(ID id, T entity) {
+        return repository.save(entity);
+    }
+
+    public void delete(ID id) {
+        repository.deleteById(id);
+    }
+}
