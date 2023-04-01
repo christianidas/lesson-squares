daa57ef67259c210001c8146ab03fce455060a54
Christian Baird <christian.a.baird@gmail.com>
2023-04-01 00:15:13 -0400

    Changing all person classes to extend our reusable crud classes and removing a bunch of extra code

--- src/main/java/org/example/Person/controller/PersonController.java
+++ src/main/java/org/example/Person/controller/PersonController.java
@@ -2,41 +2,13 @@ package org.example.Person.controller;
 
 import org.example.Person.entity.Person;
 import org.example.Person.service.PersonService;
+import org.example.crud.controller.CrudController;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/person")
-public class PersonController {
-    private final PersonService personService;
-
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
-
-    @PostMapping("")
-    public Person create(@RequestBody Person person) {
-        return personService.create(person);
-    }
-
-    @GetMapping("")
-    public List<Person> getAll() {
-        return personService.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public Person get(@PathVariable int id) {
-        return personService.get(id);
-    }
-
-    @PutMapping("/{id}")
-    public Person update(@PathVariable int id, @RequestBody Person person) {
-        return personService.update(id, person);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable int id) {
-        personService.delete(id);
+public class PersonController extends CrudController<Person, Integer> {
+    public PersonController(PersonService service) {
+        super(service);
     }
 }
