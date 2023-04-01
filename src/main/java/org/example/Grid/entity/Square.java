a58a73fb2e475a3df9afad38e774d7c7e4144419
Christian Baird <christian.a.baird@gmail.com>
2023-03-31 22:55:32 -0400

    Registering Square class as an entity, adding id and making owner transient for now

--- src/main/java/org/example/Grid/entity/Square.java
+++ src/main/java/org/example/Grid/entity/Square.java
@@ -1,14 +1,28 @@
 package org.example.Grid.entity;
 
+import jakarta.persistence.*;
 import org.example.Person.entity.Person;
 
+@Entity
 public class Square {
-    int row;
-    int col;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    private int row;
+    private int col;
+    @Transient
     private Person owner;
 
     public Square() {
 
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public int getRow() {
