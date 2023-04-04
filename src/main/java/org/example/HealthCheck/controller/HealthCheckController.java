976d1cee38374fbce9a9f2c314e18038c84cc54b
Christian Baird <christian.a.baird@gmail.com>
2023-04-03 23:06:00 -0400

    Changing the path for our healthcheck to free up the root path for our html

--- src/main/java/org/example/HealthCheck/controller/HealthCheckController.java
+++ src/main/java/org/example/HealthCheck/controller/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/")
+    @GetMapping("/healthcheck")
     public String index() {
         return "App is up and running!";
     }
