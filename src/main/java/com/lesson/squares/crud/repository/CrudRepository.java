e6898937dbd42575eff68334e705a154708442bf
Christian Baird <christian.a.baird@gmail.com>
2023-04-05 00:24:47 -0400

    Changing our CrudRepository interface to extend MongoRepository

--- src/main/java/com/lesson/squares/crud/repository/CrudRepository.java
+++ src/main/java/com/lesson/squares/crud/repository/CrudRepository.java
@@ -1,10 +1,11 @@
 package com.lesson.squares.crud.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
 @NoRepositoryBean
-public interface CrudRepository<T, ID> extends org.springframework.data.repository.CrudRepository<T, ID> {
+public interface CrudRepository<T, ID> extends MongoRepository<T, ID> {
     List<T> findAll();
 }
